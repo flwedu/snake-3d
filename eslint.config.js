5a665abfc0f36b09ba3241891122995cde1968b7
Eduardo Aquino <edutraquino@gmail.com>
2025-07-19 20:00:54 -0300

    🔧 chore(eslint.config.js): melhora a formatação e adiciona regras personalizadas
    Este commit reformata o arquivo de configuração do ESLint para melhor legibilidade e adiciona regras personalizadas para desativar avisos específicos do React e TypeScript, melhorando a experiência de desenvolvimento.

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,29 @@ import pluginReact from "eslint-plugin-react";
 import markdown from "@eslint/markdown";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+	},
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		languageOptions: { globals: globals.browser },
+	},
+	tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	{
+		files: ["**/*.md"],
+		plugins: { markdown },
+		language: "markdown/gfm",
+		extends: ["markdown/recommended"],
+	},
+	{
+		rules: {
+			"react/react-in-jsx-scope": "off",
+			"react/no-unknown-property": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+		},
+	},
 ]);
